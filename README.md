72b37bd6bfcefce768c186db29243c16e181c327
Iqbal <iqbalmineraltown@gmail.com>
2019-02-26 17:49:14 +0700

    add fastlane dependency

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # docker-images-flutter
-Docker Images for Flutter
+
+Docker Images for Flutter with Fastlane
+
+## build image
+
+```bash
+export FLUTTER_VERSION=<flutter_version> && ./build_docker.sh
+```
--- build_docker.sh
+++ build_docker.sh
@@ -5,8 +5,8 @@ set -e
 # pull to make sure we are not rebuilding for nothing
 docker pull cirrusci/flutter:base
 
-docker build --tag cirrusci/flutter:base base
-docker build --tag cirrusci/flutter:$FLUTTER_VERSION \
-             --tag cirrusci/flutter:latest \
+docker build --tag oval/flutter:base base
+docker build --tag oval/flutter:$FLUTTER_VERSION \
+             --tag oval/flutter:latest \
              --build-arg flutter_version=$FLUTTER_VERSION \
              sdk
